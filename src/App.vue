549a6dc481743db971c5b9936ef63f37865b55cc
Trevor Jones <tjones@lucidyne.com>
2017-08-08 16:46:04 -0700

    added sidenav and toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,59 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <v-app id="example-3" standalone>
+      <v-navigation-drawer permanent clipped light>
+        <v-list dense class="pt-0">
+          <v-list-tile v-for="item in items" :key="item.title" router exact :href='item.path'>
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar dark class="primary">
+        <v-toolbar-title class="white--text">Recipe App</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>apps</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>refresh</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <main>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </main>
+    </v-app>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-};
+  data () {
+    return {
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'dashboard', path: '/' },
+        { title: 'About', icon: 'question_answer', path: '/about' }
+      ],
+      right: null
+    }
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Homepage from '@/components/Homepage';
+import Homepage0 from '@/components/Homepage0';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Homepage',
       component: Homepage,
+    },
+    {
+      path: '/about/',
+      name: 'Homepage',
+      component: Homepage0,
     },
   ],
 });
