bc2b853fb24af09e4fa7324d222cf6af367ce8ce
Trevor-Jones <trevor.eric.j@gmail.com>
2017-08-11 14:51:20 -0700

    Added more fields to recipeviewer, switched to card layout, need to put times someplace better

--- src/components/RecipeViewer.vue
+++ src/components/RecipeViewer.vue
@@ -1,22 +1,47 @@
 <template>
   <div>
-    <h2>{{this.recipe.name}}</h2>
-    <v-list two-line subheader>
-      <h5>Ingredients</h5>
-      <v-layout row wrap>
-        <v-flex xs6 v-for="ingredients in twoIngredientLists">
-          <v-list-tile v-for="ingredient in ingredients" @click.native='ingredient.checked = !ingredient.checked'>
-            <v-list-tile-action>
-              <v-checkbox v-on:click.stop.prevent="ingredient.checked = !ingredient.checked" color='primary' v-model="ingredient.checked"></v-checkbox>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ingredient.name}}</v-list-tile-title>
-              <v-list-tile-sub-title>{{ingredient.amount + ' ' + ingredient.units}}</v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
+    <v-layout>
+      <v-flex xs12 sm8 offset-sm2>
+        <v-card>
+          <v-card-media :src="recipe.image" height="200px"></v-card-media>
+          <v-card-title primary-title>
+              <v-flex xs6>
+                <h3 class="display-2 mb-0" style="padding-bottom: 10px">{{recipe.name}}</h3>
+                <div>{{recipe.introduction}}</div>
+              </v-flex>
+
+              <v-flex xs6>
+                <p align='right'>Prep Time: {{recipe.prepTime}}</p>
+                <p align='right'>Cook Time: {{recipe.cookTime}}</p>
+              </v-flex>
+          </v-card-title>
+
+          <v-card-text>
+            <v-list two-line subheader>
+              <v-layout row wrap>
+                <v-flex xs6>
+                  <h5>Ingredients</h5>
+                  <v-list-tile v-for="ingredient in recipe.ingredients" @click.native='ingredient.checked = !ingredient.checked'>
+                    <v-list-tile-action>
+                      <v-checkbox v-on:click.stop.prevent="ingredient.checked = !ingredient.checked" color='primary' v-model="ingredient.checked"></v-checkbox>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>{{ingredient.name}}</v-list-tile-title>
+                      <v-list-tile-sub-title>{{ingredient.amount + ' ' + ingredient.units}}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-flex>
+
+                <v-flex xs6>
+                  <h5>Instructions</h5>
+                  <p>{{recipe.instructions}}</p>
+                </v-flex>
+              </v-layout>
+            </v-list>
+          </v-card-text>
+          </v-card>
         </v-flex>
       </v-layout>
-    </v-list>
   </div>
 </template>
 
@@ -47,21 +72,6 @@
         self.recipe = snapshot.val()
       });
     },
-    computed: {
-      twoIngredientLists() {
-        var lists = [[],[]];
-        var listNum = 0;
-        this.recipe.ingredients.forEach(function(ingredient) {
-          lists[listNum].push(ingredient);
-          if(listNum == 0) {
-            listNum = 1;
-          } else {
-            listNum = 0;
-          }
-        });
-        return lists
-      }
-    }
   };
 </script>
 
